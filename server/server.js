324de8cfe6968b0365b224421f1318bdcd24dbcd
dari <kulikovadariia@ukr.net>
2020-05-08 18:28:02 +0300

    change server

--- server/server.js
+++ server/server.js
@@ -81,11 +81,13 @@ server.delete('/api/v1/users/:userId', async (req, res) => {
   res.json({ status: 'success', id: +userId })
 })
 
-server.delete('/api/v1/users', (req, res) => {
+server.delete('/api/v1/users/', (req, res) => {
   stat(`${__dirname}/${filename}`)
-    .then(() => unlink(`${__dirname}/${filename}`))
-    .catch(() => res.json({ status: 'ok' }))
-  res.json({ status: 'ok' })
+    .then(() => {
+      unlink(`${__dirname}/${filename}`)
+      res.json({ status: 'ok' })
+    })
+    .catch((err) => res.json({ status: 'Error', ErrorMessage: err }))
 })
 
 server.use('/api/', (req, res) => {
